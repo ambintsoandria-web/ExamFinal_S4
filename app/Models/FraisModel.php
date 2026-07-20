2cc2e4d70f705f4c136f86e716c28351219fde70
Randriamirado Alan Solofoniaina <alanrandriamirado@gmail.com>
2026-07-20 08:33:59 +0200

    frais
    frais

--- app/Models/FraisModel.php
+++ app/Models/FraisModel.php
@@ -15,4 +15,21 @@ class FraisModel extends Model
         'montant_max',
         'montant_frais'
     ];
+
+    public function getFraisByTypeOperation($typeOperationId)
+    {
+        return $this->where('type_operation_id', $typeOperationId)->findAll();
+    }
+    public function deleteFrais($fraisId)
+    {
+        return $this->delete($fraisId);
+    }
+    public function updateFrais($fraisId, $data)
+    {
+        return $this->update($fraisId, $data);
+    }
+    public function getFraisById($fraisId)
+    {
+        return $this->find($fraisId);
+    }
 }
